4954cbc17b7d3fa74b397c232cff303953a03188
ShalynishkaPanda <mr.romaxa2@mail.ru>
2021-12-18 20:02:50 +0300

    Fourth commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("The most popular");
-        System.out.println("World");
+        System.out.println("10 dollars");
         System.out.println("Zoo");
     }
 }
