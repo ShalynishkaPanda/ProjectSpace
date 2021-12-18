634fc5ab4520c9fa70a4977fcae4b0ae02a5b861
AnnaAvl <86964707+AnnaAvl@users.noreply.github.com>
2021-12-18 20:24:42 +0300

    Ann create new branch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,15 @@ package com.company;
 
 public class Main {
 
+    public static void project(){
+        System.out.println("Start project!");
+    }
+
     public static void main(String[] args) {
         System.out.println("The most popular");
         System.out.println("10 dollars");
         System.out.println("Zoo");
         System.out.println("Hello from Ann!");
+        project();
     }
 }
